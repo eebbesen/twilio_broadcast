027f183f4cf34a9b3700fec50404f8dd0afd3b7b
eebbesen <eebbesen.git@gmail.com>
2020-09-02 08:38:28 -0500

    break out test

--- spec/views/recipient_lists/show.html.erb_spec.rb
+++ spec/views/recipient_lists/show.html.erb_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'recipient_lists/show', type: :view do
     user = create(:user_1)
     recipient = create(:recipient_1, user: user)
     @recipient_list = assign(:recipient_list, RecipientList.create!(
-                                                name: 'Name',
+                                                name: 'Zoning',
                                                 notes: 'Notes',
                                                 user_id: user.id
                                               ))
@@ -17,7 +17,12 @@ RSpec.describe 'recipient_lists/show', type: :view do
   it 'renders attributes in <p>' do
     render
     expect(rendered).to match(/Name/)
+    expect(rendered).to match(/Zoning/)
     expect(rendered).to match(/Notes/)
+  end
+
+  it 'renders list members' do
+    render
     expect(rendered).to match(/Members/)
     expect(rendered).to match(/Omar/)
     expect(rendered).to match(/0008675309/)
