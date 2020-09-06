2950cff04953899447c59017a4b2d664c1af157b
eebbesen <eebbesen.git@gmail.com>
2020-09-06 14:22:25 -0500

    rubocop

--- app/services/twilio_text_messenger.rb
+++ app/services/twilio_text_messenger.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+##
 class TwilioTextMessenger
   attr_reader :content
 
@@ -8,9 +11,9 @@ class TwilioTextMessenger
   def call(recipient)
     client = Twilio::REST::Client.new
     client.messages.create({
-      from: ENV['TWILIO_FROM_PHONE_NUMBER'],
-      to: recipient,
-      body: content
-    })
+                             from: ENV['TWILIO_FROM_PHONE_NUMBER'],
+                             to: recipient,
+                             body: content
+                           })
   end
 end
--- spec/services/twilio_text_messenger_spec.rb
+++ spec/services/twilio_text_messenger_spec.rb
@@ -15,37 +15,31 @@ RSpec.describe TwilioTextMessenger do
 
   # uses the invalid Twilio magic to number
   it 'sends message to invalid number' do
-    begin
-      r = TwilioTextMessenger.new('test message').call('+15005550001')
-    rescue => e
-      expect(r).to be_nil
-      expect(e.status_code).to eq(400)
-      expect(e.code).to eq(21211)
-      expect(e.error_message).to eq("The 'To' number +15005550001 is not a valid phone number.")
-    end
+    r = TwilioTextMessenger.new('test message').call('+15005550001')
+  rescue StandardError => e
+    expect(r).to be_nil
+    expect(e.status_code).to eq(400)
+    expect(e.code).to eq(21_211)
+    expect(e.error_message).to eq("The 'To' number +15005550001 is not a valid phone number.")
   end
 
   # uses the blocked Twilio magic to number
   it 'sends message to blocked number' do
-    begin
-      r = TwilioTextMessenger.new('test message').call('+15005550004')
-    rescue => e
-      expect(r).to be_nil
-      expect(e.status_code).to eq(400)
-      expect(e.code).to eq(21610)
-      expect(e.error_message).to eq('The message From/To pair violates a blacklist rule.')
-    end
+    r = TwilioTextMessenger.new('test message').call('+15005550004')
+  rescue StandardError => e
+    expect(r).to be_nil
+    expect(e.status_code).to eq(400)
+    expect(e.code).to eq(21_610)
+    expect(e.error_message).to eq('The message From/To pair violates a blacklist rule.')
   end
 
   # uses the Twilio magic to number that can't receive text messages
   it "sends message to number that can't receive text messages" do
-    begin
-      r = TwilioTextMessenger.new('test message').call('+15005550009')
-    rescue => e
-      expect(r).to be_nil
-      expect(e.status_code).to eq(400)
-      expect(e.code).to eq(21614)
-      expect(e.error_message).to eq('To number: +15005550009, is not a mobile number')
-    end
+    r = TwilioTextMessenger.new('test message').call('+15005550009')
+  rescue StandardError => e
+    expect(r).to be_nil
+    expect(e.status_code).to eq(400)
+    expect(e.code).to eq(21_614)
+    expect(e.error_message).to eq('To number: +15005550009, is not a mobile number')
   end
 end
