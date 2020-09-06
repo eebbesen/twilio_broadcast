e0a2d424ed9f79d5d3f80d1cad88fd1eb726907b
eebbesen <eebbesen.git@gmail.com>
2020-09-06 14:37:26 -0500

    rubocop

--- app/services/twilio_text_messenger.rb
+++ app/services/twilio_text_messenger.rb
@@ -11,9 +11,9 @@ class TwilioTextMessenger
   def call(recipient)
     client = Twilio::REST::Client.new
     client.messages.create(
-                             from: ENV['TWILIO_FROM_PHONE_NUMBER'],
-                             to: recipient,
-                             body: content
-                           )
+      from: ENV['TWILIO_FROM_PHONE_NUMBER'],
+      to: recipient,
+      body: content
+    )
   end
 end
