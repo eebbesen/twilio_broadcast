e14984b3102af89d190c6acf746f6ad7626cf5d7
eebbesen <eebbesen.git@gmail.com>
2020-10-05 22:33:45 -0500

    handle status posts

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,15 @@
 ##
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[show edit update destroy send_message]
+  before_action :authenticate_user!, except: 'sms_status'
+
+  # POST /messages/sms_status
+  def sms_status
+    mr = MessageRecipient.where(sid: params['SmsSid'])
+    return unless status_update_valid? mr
+
+    mr.first.update(status: params['SmsStatus'])
+  end
 
   # GET /messages
   # GET /messages.json
@@ -77,6 +86,16 @@ class MessagesController < ApplicationController
 
   private
 
+  def status_update_valid?(recipient)
+    return false unless recipient.first
+
+    unless "+#{recipient.first.recipient.phone}" == params['To']
+      logger.warn("No match for sid #{params['SmsSid']} and phone #{params['To']}")
+      return false
+    end
+    true
+  end
+
   def send_recipients
     @message.recipient_lists.map(&:recipients).flatten.uniq.each do |r|
       send_recipient(r)
@@ -95,8 +114,7 @@ class MessagesController < ApplicationController
     store_recipient_send(recipient, { status: result.status,
                                       error_code: result.error_code,
                                       error_message: result.error_message,
-                                      sid: result.sid
-                                    })
+                                      sid: result.sid })
   end
 
   def store_recipient_send(recipient, details = {})
