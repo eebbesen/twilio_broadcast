a4103e9d2eac85c2df0d561c562f6ba140f3862c
J2EEbbesen <eebbesen@users.noreply.github.com>
2020-11-19 07:27:24 -0600

    Delete testing (#52)
    * updating rspec
    
    * testing
    
    * more deletion testing

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -3,24 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Messages', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  before { driven_by(:rack_test) }
+  let(:user) { create(:user_1) }
 
   context 'signed in user' do
     before do
-      @user = create(:user_1)
-      pword = 'Passw0rd!'
       visit 'users/sign_in'
-      fill_in 'Email', with: @user.email
-      fill_in 'Password', with: pword
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: user.password
       click_on 'Log in'
     end
 
     it 'allows message creation' do
-      rl1 = create(:recipient_list_1, user: @user)
-      rl2 = create(:recipient_list_2, user: @user)
-
+      rl1 = create(:recipient_list_1, user: user)
+      rl2 = create(:recipient_list_2, user: user)
       text = "This is a test message from #{DateTime.now.strftime('%I:%M:')}"
 
       visit '/messages'
@@ -49,6 +45,52 @@ RSpec.describe 'Messages', type: :system do
 
       expect(page).to have_text "Content can't be blank"
       expect(page).to have_text 'Content is too short'
+    end
+
+    context 'with recipients' do
+      let(:rl1) { create(:recipient_list_1, user: user) }
+      let(:rl2) { create(:recipient_list_2, user: user) }
+
+      context 'existing message' do
+        let(:message) { create(:message, user: user, content: 'hello there') }
+        before do
+          rl1.recipients << create(:recipient, user: user, phone: '+16515551212')
+          message.recipient_lists << rl1
+          message.save!
+        end
+
+        it 'deletes unsent message from index' do
+          expect do
+            visit '/messages'
+            click_on 'Delete'
+            expect(page).to have_text('Message was successfully destroyed')
+            expect(page).not_to have_text(message.content)
+          end.to change(Message, :count).by(-1)
+        end
+
+        it 'deletes unsent message from show' do
+          expect do
+            visit "/messages/#{message.id}"
+            click_on 'Delete'
+            expect(page).to have_text('Message was successfully destroyed')
+            expect(page).not_to have_text(message.content)
+          end.to change(Message, :count).by(-1)
+        end
+
+        it "can't delete sent message from index" do
+          message.update_attribute(:status, 'Sent')
+          visit '/messages'
+          expect(page).not_to have_text('Delete')
+          expect(page).to have_text(message.content)
+        end
+
+        it "can't delete sent message from show" do
+          message.update_attribute(:status, 'Sent')
+          visit "/messages/#{message.id}"
+          expect(page).not_to have_text('Delete')
+          expect(page).to have_text(message.content)
+        end
+      end
     end
   end
 end
